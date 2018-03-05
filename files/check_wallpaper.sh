e6ce2454b0e9082b135538598617ce93db637c74
Lars Boegild Thomsen <lth@netcompartner.com>
2018-03-05 16:05:46 +0800

    Working I think

--- files/check_wallpaper.sh
+++ files/check_wallpaper.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-echo "foo" >/tmp/check_wallpaper.run
+if [ -f ~/wallpaper.jpg ]; then
+    sleep 10
+    mv ~/wallpaper.jpg ~/.config
+    gsettings set org.mate.background picture-filename ~/.config/wallpaper.jpg
+fi
+# vim: ts=4 et nowrap autoindent
